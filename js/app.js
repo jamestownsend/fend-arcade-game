579ca018a93bf988e0bd5f47ff461004639a71b5
James Townsend <jtownsend@pc.factset.com>
2018-06-16 12:02:23 +0100

    Added collision function

--- js/app.js
+++ js/app.js
@@ -17,8 +17,32 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    // start enemy back at the original position once off canvas.
+    if (this.x > 505) {
+      this.x = -100;
+      this.speed = 100 + Math.floor(Math.random() * 20);
+    }
+    this checkCollision();
 };
 
+Enemy.prototype.checkCollision = function() {
+    //creates collision zones for player and enemy
+    let playerZone = {width: 50, height: 45, x: player.x, y: player.y};
+    let enemyZone = {width: 55, height: 50, x: this.x, y: player.y};
+
+    if (playerZone.x < enemyZone.x + enemyZone.width &&
+        playerZone.x + playerZone.width > enemyZone.x &&
+        playerZone.y < enemyZone.y + enemyZone.height &&
+        playerZone.height + playerZone.y > enemyZone.y) {
+        //resets player at start upon collision;
+        player.x = 200;
+        player.y = 380;
+    }
+};
+
+    
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
