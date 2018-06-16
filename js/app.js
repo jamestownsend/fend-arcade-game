0f2fcdb99f39a299ebbd6e2cdd893747080c05cd
James Townsend <jtownsend@pc.factset.com>
2018-06-16 11:28:57 +0100

    Added player and enemy initial variables

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+let Enemy = function(speed, x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.speed = speed;
+    this.x = x;
+    this.y = y;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -22,9 +25,14 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
+let Player = function(speed, x, y) {
 // This class requires an update(), render() and
 // a handleInput() method.
+  this.speed = speed;
+  this.x = x;
+  this.y = y;
 
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
