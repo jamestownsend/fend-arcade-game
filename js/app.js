f7f1dc775f339b0fb185604ddc670985af7cdd12
James Townsend <jtownsend@pc.factset.com>
2018-07-08 15:57:30 +0100

    Added collision detection

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 // Enemies our player must avoid
-let Enemy = function(speed, x, y) {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.speed = speed;
     this.x = x;
     this.y = y;
-
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -20,23 +19,30 @@ Enemy.prototype.update = function(dt) {
     this.x += this.speed * dt;
 
     // start enemy back at the original position once off canvas.
-    if (this.x > 505) {
+    if (this.x > 600) {
       this.x = -100;
-      this.speed = 100 + Math.floor(Math.random() * 20);
+      this.speed = 95 + Math.floor(Math.random() * 1028);
     }
-    this checkCollision();
+
+    this.checkCollision();
 };
+
+// Collision code adapted from:
+// https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
 
 Enemy.prototype.checkCollision = function() {
     //creates collision zones for player and enemy
-    let playerZone = {width: 50, height: 45, x: player.x, y: player.y};
-    let enemyZone = {width: 55, height: 50, x: this.x, y: player.y};
+    let playerZone = { x: player.x, y: player.y, width: 50, height: 50 };
+    let enemyZone = { x: this.x, y: this.y, width: 50, height: 50 };
 
     if (playerZone.x < enemyZone.x + enemyZone.width &&
         playerZone.x + playerZone.width > enemyZone.x &&
         playerZone.y < enemyZone.y + enemyZone.height &&
         playerZone.height + playerZone.y > enemyZone.y) {
         //Resets player at start upon collision;
+        document.body.style.backgroundColor = "red";
+        setTimeout(function(){document.body.style.backgroundColor = "white";}, 400);
+        setTimeout(function(){alert("Ouch, Try again!");}, 400);
         player.x = 200;
         player.y = 380;
     }
@@ -49,16 +55,17 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
-let Player = function(speed, x, y) {
+var Player = function(x, y, speed) {
 // This class requires an update(), render() and
 // a handleInput() method.
-  this.speed = speed;
-  this.x = x;
-  this.y = y;
+this.x = x;
+this.y = y;
+this.speed = speed;
 // The image/sprite for our player, this uses
   this.sprite = 'images/char-boy.png';
 }
 
+//Stops player from being able to move outside of the game map.
 Player.prototype.update = function() {
 
     if (this.y > 380) {
@@ -79,19 +86,23 @@ Player.prototype.update = function() {
     }
 };
 
-Player.prototype.handleInput = function(allowedKeys) {
-    switch (allowedKeys) {
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPress) {
+    switch (keyPress) {
         case 'left':
-            this.x -= this.speed + 40;
+            this.x -= this.speed + 50;
             break;
         case 'up':
-            this.y -= this.speed + 25;
+            this.y -= this.speed + 30;
             break;
         case 'right':
-            this.x += this.speed + 40;
+            this.x += this.speed + 50;
             break;
         case 'down':
-            this.y += this.speed + 25;
+            this.y += this.speed + 30;
             break;
     }
 };
@@ -100,13 +111,23 @@ Player.prototype.handleInput = function(allowedKeys) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+
+var enemyPosition = [60, 140, 220];
+var player = new Player(200, 380, 50);
+var enemy;
+
+enemyPosition.forEach(function(posY) {
+    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 258));
+    allEnemies.push(enemy);
+});
 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
